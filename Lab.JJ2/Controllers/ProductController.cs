5ffb9540999db1e6067bd8724b177ac42c2d7718
Zachary Hordynski <zach.hordynski@gmail.com>
2019-01-14 09:35:26 -0500

    My first MVC web app

---
+++ Lab.JJ2/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Lab.JJ2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.JJ2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab.JJ2/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Lab.JJ2.Models
+{
+    public class Product
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Category { get; set; }
+    }
+}
